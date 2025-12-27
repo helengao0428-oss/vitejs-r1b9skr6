1789e2053034843bf09076180e0b511e937e687e
helengao0428-oss <helengao0428@gmail.com>
2025-12-27 23:58:47 +0800

    fix: add base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 加入這行，確保資源路徑正確
 })
